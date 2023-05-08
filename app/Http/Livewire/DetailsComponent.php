8efefbdc77d1ade7119d0a8a7bc0106c546b4351
julio-DevI <julio.a.valenca@gmail.com>
2023-05-08 08:28:07 -0300

    make details component code

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class DetailsComponent extends Component
 {
+    public $slug;
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.details-component');
+        $product = Product::where('slug',$this->slug)->first();
+        return view('livewire.details-component',['product' => $product]);
     }
 }
