b3085b8084860f1ba4113cda02ecf11cc6a7e64b
julio-DevI <julio.a.valenca@gmail.com>
2023-05-11 16:28:30 -0300

    create Cart icon component

---
+++ app/Http/Livewire/CartIconComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CartIconComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.cart-icon-component');
+    }
+}
