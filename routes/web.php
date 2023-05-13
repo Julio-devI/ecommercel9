fb165284ec91dd4fdd648ad1e83979d08602f8ee
julio-DevI <julio.a.valenca@gmail.com>
2023-05-13 08:12:32 -0300

    implement search product

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,8 @@ Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
 Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
+
+Route::get('/search',SearchComponent::class)->name('product.search');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
