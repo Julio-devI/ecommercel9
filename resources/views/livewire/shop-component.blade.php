84bda7c71589e749230af91d10a7078b8e7aaa41
julio-DevI <julio.a.valenca@gmail.com>
2023-05-13 07:24:59 -0300

    create category component

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -121,13 +121,9 @@
                         <div class="widget-category mb-30">
                             <h5 class="section-title style-1 mb-30 wow fadeIn animated">Category</h5>
                             <ul class="categories">
-                                <li><a href="shop.html">Shoes & Bags</a></li>
-                                <li><a href="shop.html">Blouses & Shirts</a></li>
-                                <li><a href="shop.html">Dresses</a></li>
-                                <li><a href="shop.html">Swimwear</a></li>
-                                <li><a href="shop.html">Beauty</a></li>
-                                <li><a href="shop.html">Jewelry & Watch</a></li>
-                                <li><a href="shop.html">Accessories</a></li>
+                            @foreach ($categories as $category)
+                                <li><a href="shop.html">{{$category->name}}</a></li>
+                            @endforeach
                             </ul>
                         </div>
                         <!-- Fillter By Price -->
