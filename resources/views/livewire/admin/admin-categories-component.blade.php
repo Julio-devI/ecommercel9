03e6840f718f5e74e23638c005c8e17f2fd6ffb9
julio-DevI <julio.a.valenca@gmail.com>
2023-05-15 15:38:41 -0300

    make the add the category page admin

--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -23,7 +23,15 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
-                                All categories
+                                <div class="row">
+                                    <div class="col-md-6 ">
+                                        All categories
+                                    </div>
+
+                                    <div class="col-md-6 ">
+                                        <a href="{{route('admin.category.add')}}" class="btn btn-success float-end">Add new category</a>
+                                    </div>
+                                </div>
                             </div>
                             <div class="card-body">
                                 <table class="table table-striped">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
@@ -56,6 +57,7 @@ Route::middleware(['auth', 'authadmin'])->group(function ()
 {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoriesComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.category.add');
 });
 
 require __DIR__.'/auth.php';
