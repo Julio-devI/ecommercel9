501e68c3fd2b56acd530d25c3bddc80e6752f076
julio-DevI <julio.a.valenca@gmail.com>
2023-05-15 09:03:25 -0300

    Show all wishlisted products

--- app/Http/Livewire/WishlistComponent.php
+++ app/Http/Livewire/WishlistComponent.php
@@ -3,11 +3,25 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class WishlistComponent extends Component
 {
     public function render()
     {
         return view('livewire.wishlist-component');
+    }
+
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id == $product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-icon-component', 'refreshComponent');
+                return;
+            }
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,8 @@ Route::get('/shop', ShopComponent::class)->name('shop');
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 Route::get('/cart', CartComponent::class)->name('shop.cart');
+
+Route::get('/wishlist', WishlistComponent::class)->name('shop.wishlist');
 
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 
