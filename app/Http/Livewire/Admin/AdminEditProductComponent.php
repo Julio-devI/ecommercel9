d632d11a9e53a623543e0a1bacacbeaeffa0458d
julio-DevI <julio.a.valenca@gmail.com>
2023-05-20 15:06:38 -0300

    create edit product component

---
+++ app/Http/Livewire/Admin/AdminEditProductComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminEditProductComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-edit-product-component');
+    }
+}
