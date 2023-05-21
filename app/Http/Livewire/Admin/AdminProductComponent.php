e8e2d733ca5deeac7cb215715c1580cf7c94eecb
julio-DevI <julio.a.valenca@gmail.com>
2023-05-21 19:23:11 -0300

    delete function for admin

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,6 +9,17 @@ use Livewire\WithPagination;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+
+    public $product_id;
+
+    public function deleteProduct()
+    {
+        $product = Product::find($this->product_id);
+        unlink('assets/imgs/products/'.$product->image);
+        $product->delete();
+        session()->flash('message','Product has been deleted successfully !');
+    }
+
     public function render()
     {
         $products = Product::orderBy('created_at','DESC')->paginate(10);
--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -64,6 +64,7 @@
                                                 <td>{{$product->created_at}}</td>
                                                 <td>
                                                     <a href="{{route('admin.product.edit',['product_id'=>$product->id])}}" class="text-info">Edit</a>
+                                                    <a href="#" onclick="deleteConfirmation({{$product->id}})" class="text-danger">Delete</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -78,3 +79,35 @@
         </section>
     </main>
 </div>
+
+<div class="modal" id="deleteConfirmation">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body pb-30 pt-30">
+                <div class="row">
+                    <div class="text-center col-md-12">
+                        <h4 class="pb-3">Do you want to delete this record ?</h4>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
+                        <button type="button" class="btn btn-danger" onclick="deleteProduct()">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        function deleteConfirmation(id)
+        {
+            @this.set('product_id',id);
+            $('#deleteConfirmation').modal('show');
+        }
+
+        function deleteProduct()
+        {
+            @this.call('deleteProduct');
+            $('#deleteConfirmation').modal('hide');
+        }
+    </script>
+@endpush
