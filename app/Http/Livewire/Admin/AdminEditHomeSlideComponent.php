a0dae9f7176de84f716304b89e16d71e75fde9d3
julio-DevI <julio.a.valenca@gmail.com>
2023-05-22 13:28:08 -0300

    create the admin edit home slide component

---
+++ app/Http/Livewire/Admin/AdminEditHomeSlideComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminEditHomeSlideComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-edit-home-slide-component');
+    }
+}
